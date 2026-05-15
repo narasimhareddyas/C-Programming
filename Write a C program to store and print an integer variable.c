80aa3df3fa8b6f2dbddb6156cd23155b2c5f454e
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-05-15 19:03:31 +0530

    Add files via upload

---
+++ Write a C program to store and print an integer variable.c
@@ -0,0 +1,17 @@
+//Write a C program to store and print an integer variable.//
+
+#include <stdio.h>
+
+int main()
+{
+    int s;   // Integer variable
+
+    // Taking integer input from user
+    printf("Enter the integer: ");
+    scanf("%d", &s);
+
+    // Printing the integer
+    printf("s: %d\n", s);
+
+    return 0;
+}
