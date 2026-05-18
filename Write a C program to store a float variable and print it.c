1745e9844a2b445d96c0eaefa7460c1540aa16ee
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-05-18 18:18:35 +0530

    Add files via upload

---
+++ Write a C program to store a float variable and print it.c
@@ -0,0 +1,11 @@
+//Write a C program to store a float variable and print it.
+
+#include<stdio.h>
+int main()
+{
+float num;
+printf("Enter the float num:");
+scanf("%f",&num);
+printf("float num=%.2f\n",num);
+return 0;
+}
