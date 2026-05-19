0c041b87e1e6e5d99eb72df8dc670850c8440270
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-05-19 18:44:10 +0530

    Add files via upload

---
+++ Write a C program to print the size of different data types using sizeof().c
@@ -0,0 +1,16 @@
+//Write a C program to print the size of different data types using sizeof().
+
+#include <stdio.h>
+
+int main()
+{
+    // Printing size of different data types
+
+    printf("Size of int = %d\n", sizeof(int));
+    printf("Size of char = %d\n", sizeof(char));
+    printf("Size of float = %d\n", sizeof(float));
+    printf("Size of double = %d\n", sizeof(double));
+    printf("Size of long double = %d\n", sizeof(long double));
+
+    return 0;
+}
