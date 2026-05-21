b11cdba8901c6b2fb2cd2f88369302d5e697b321
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-05-21 17:19:08 +0530

    Add files via upload

---
+++ Write a C program to convert integer to float.c
@@ -0,0 +1,16 @@
+//Write a C program to convert integer to float.
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 22;      // Integer variable
+
+    // Integer converted to float
+    float value = num;
+
+    // Printing float value
+    printf("float value = %.2f\n", value);
+
+    return 0;
+}
