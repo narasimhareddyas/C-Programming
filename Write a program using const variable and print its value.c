b5dcf79e98e47d31f5fddd3b7b0126687bde7eaf
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-05-27 16:57:58 +0530

    Add files via upload

---
+++ Write a program using const variable and print its value.c
@@ -0,0 +1,14 @@
+//Write a program using const variable and print its value.
+
+#include <stdio.h>
+
+int main()
+{
+    // Constant integer variable
+    const int num = 22;
+
+    // Printing constant value
+    printf("num = %d\n", num);
+
+    return 0;
+}
