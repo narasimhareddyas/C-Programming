fa5c5fb8089f6cd46657c71a5a2749e3424912f1
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-05-29 18:25:30 +0530

    Add files via upload

---
+++ Write a program to compare signed and unsigned variables.c
@@ -0,0 +1,20 @@
+//Write a program to compare signed and unsigned variables.
+
+#include <stdio.h>
+
+int main()
+{
+    // Signed integer
+    int a = -36;
+
+    // Unsigned integer
+    unsigned int b = 36;
+
+    // Printing signed value
+    printf("Signed a = %d\n", a);
+
+    // Printing unsigned value
+    printf("Unsigned b = %u\n", b);
+
+    return 0;
+}
