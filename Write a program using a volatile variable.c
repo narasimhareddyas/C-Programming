c75097c9d58cb0090a252dcb6aa39b09c3b3b91b
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-05-29 18:25:05 +0530

    Add files via upload

---
+++ Write a program using a volatile variable.c
@@ -0,0 +1,14 @@
+//Write a program using a volatile variable.
+
+#include <stdio.h>
+
+int main()
+{
+    // Volatile integer variable
+    volatile int a = 22;
+
+    // Printing value
+    printf("a = %d\n", a);
+
+    return 0;
+}
