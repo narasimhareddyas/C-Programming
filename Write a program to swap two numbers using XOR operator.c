575a26866077708a37ae052aca4130c055980cf4
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-06-01 18:00:04 +0530

    Add files via upload

---
+++ Write a program to swap two numbers using XOR operator.c
@@ -0,0 +1,17 @@
+//Write a program to swap two numbers using XOR operator.
+
+#include<stdio.h>
+int main()
+{
+int num1, num2;
+printf("Enter the num1:");
+scanf("%d",&num1);
+printf("Enter the num2:");
+scanf("%d",&num2);
+num1= num1^num2;
+num2= num1^num2;
+num1= num1^num2;
+printf("num1= %d\n",num1);
+printf("num2= %d\n",num2);
+return 0;
+}
