b2cd0892d17670e5edaa40eac1d8d258c02f4c91
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-06-04 20:01:00 +0530

    Add files via upload

---
+++ Write a program to print numbers from 1 to 10 using a loop.c
@@ -0,0 +1,17 @@
+//Write a program to print numbers from 1 to 10 using a loop.
+
+#include<stdio.h>
+int main()
+{
+int a=1;
+/*for(a=1;a<=10;a++)
+{
+printf("%d\n",a);
+}*/
+while(a<=10)
+{
+    printf("%d\n",a);
+    a++;
+}
+return 0;
+}
