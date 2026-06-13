bc94ddabf2bff8c8ae3565cc1d6f1a005383d552
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-06-13 19:26:40 +0530

    Add files via upload

---
+++ Write a function to find the largest of three numbers.c
@@ -0,0 +1,25 @@
+//Write a function to find the largest of three numbers.
+
+#include <stdio.h>
+
+int largest(int a, int b, int c)
+{
+    if (a >= b && a >= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+        return c;
+}
+
+int main()
+{
+    int num1, num2, num3;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    printf("Largest number = %d", largest(num1, num2, num3));
+
+    return 0;
+}
