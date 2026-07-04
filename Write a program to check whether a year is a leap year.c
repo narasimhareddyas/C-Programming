f1a34fb3d74723c6a9ebf76e85c189998a44312a
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-07-04 21:09:08 +0530

    Add files via upload

---
+++ Write a program to check whether a year is a leap year.c
@@ -0,0 +1,18 @@
+//Write a program to check whether a year is a leap year.
+
+#include<stdio.h>
+int main()
+{
+int year;
+printf("Enter the year:");
+scanf("%d",&year);
+if((year%400==0)||(year%4==0)&&(year%100!=0))
+{
+printf("%d is leap year\n",year);
+}
+else
+{
+printf("%d is not leap year\n",year);
+}
+return 0;
+}
