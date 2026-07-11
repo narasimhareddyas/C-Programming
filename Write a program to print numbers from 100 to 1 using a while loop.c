5069bb73ef0803e792834e9f7b7f41d997dc6281
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-07-11 22:42:04 +0530

    Add files via upload

---
+++ Write a program to print numbers from 100 to 1 using a while loop.c
@@ -0,0 +1,13 @@
+//Write a program to print numbers from 100 to 1 using a while loop.
+
+#include<stdio.h>
+int main()
+{
+int num=101;
+while(num>1)
+{
+num--;
+printf("%d\n",num);
+}
+return 0;
+}
