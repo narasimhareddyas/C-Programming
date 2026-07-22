f69b2494f4e0944eb44982bf049f9ccaee75c723
Narasimhareddy A S <narasimhareddyas36@gmail.com>
2026-07-22 19:49:18 +0530

    Add files via upload

---
+++ Write a program to print Floyd's Triangle.c
@@ -0,0 +1,20 @@
+//Write a program to print Floyd's Triangle.
+
+#include<stdio.h>
+
+int main()
+{
+    int i, j, num = 1;
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
